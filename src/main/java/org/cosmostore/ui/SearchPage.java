d75b3f2a5b48aabf983e6ac08b54dc45a1516d3c
Victoria Vlasova <a5glyk@gmail.com>
2024-04-06 16:24:18 +0300

    refactor SearchPage, SearchTest

--- src/main/java/org/cosmostore/ui/SearchPage.java
+++ src/main/java/org/cosmostore/ui/SearchPage.java
@@ -1,10 +1,18 @@
 package org.cosmostore.ui;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 public class SearchPage {
     private WebDriver driver;
     public SearchPage(){ this.driver = Driver.getDriver();}
     public static final String SEARCH_INPUT = "//input[@class='form-control appended-form-control']";
-    public static final String FINDED_ELEMENT= "//div[@id='products-list']/div/div/div[@class='card-body py-2']/h3/a";
+    public static final String FOUND_ELEMENT = "//div[@id='products-list']/div/div/div[@class='card-body py-2']/h3/a";
+    public void fillSearchLineAndClickEnter(){
+        driver.findElement(By.xpath(SearchPage.SEARCH_INPUT)).sendKeys("shampoo", Keys.ENTER);
+    }
+    public String getNameOfFirstFoundElement(){
+        return driver.findElement(By.xpath(SearchPage.FOUND_ELEMENT)).getText();
+    }
 }
--- src/test/java/org/cosmostore/ui/SearchTest.java
+++ src/test/java/org/cosmostore/ui/SearchTest.java
@@ -2,14 +2,12 @@ package org.cosmostore.ui;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 
 public class SearchTest extends BaseTest {
     @Test
     public void testSearch(){
         SearchPage searchPage = new SearchPage();
-        driver.findElement(By.xpath(SearchPage.SEARCH_INPUT)).sendKeys("shampoo", Keys.ENTER);
-        String actualResult = driver.findElement(By.xpath(SearchPage.FINDED_ELEMENT)).getText();
-        actualResult.contains("shampoo");
+        searchPage.fillSearchLineAndClickEnter();
+        searchPage.getNameOfFirstFoundElement().contains("shampoo");
     }
 }
