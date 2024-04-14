4ef9fc9620e6cfe24ad540f4076c3f1ea1ac4695
Victoria Vlasova <a5glyk@gmail.com>
2024-04-14 20:29:26 +0300

    refactor

--- src/test/java/org/cosmostore/api/LoginAPITest.java
+++ src/test/java/org/cosmostore/api/LoginAPITest.java
@@ -1,4 +1,23 @@
 package org.cosmostore.api;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class LoginAPITest {
+    @Test
+    @DisplayName("with empty email and password")
+    public void testLogin() {
+        given().
+                queryParam("location", "https%3A%2F%2Fwww.cosmostore.org%2F").
+                body("email=&password=+&submit_login=true").
+                contentType("application/x-www-form-urlencoded; charset=UTF-8").
+                when().
+                post("https://www.cosmostore.org/users/login/?location=https%3A%2F%2Fwww.cosmostore.org%2F").
+                then().assertThat().
+                statusCode(200).
+                body(equalTo("{\"status\":\"error\",\"errors\":{\"email\":\"Required field Email\",\"password\":\"Invalid login information. Try to recover the password.\"}}"));
+    }
 }
