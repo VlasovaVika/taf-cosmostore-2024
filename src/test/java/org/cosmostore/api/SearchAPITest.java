ff93bbf35e98c724373ca401da4ebe37f3b8c399
Victoria Vlasova <a5glyk@gmail.com>
2024-04-14 17:10:28 +0300

    refactor

--- src/test/java/org/cosmostore/api/SearchAPITest.java
+++ src/test/java/org/cosmostore/api/SearchAPITest.java
@@ -3,10 +3,28 @@ package org.cosmostore.api;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class SearchAPITest {
     @Test
-    @DisplayName("search 'shampoo' items")
-    public void testSearch(){
-
+    @DisplayName("search 'shampoo'")
+    public void testSearch1(){
+        given().
+                queryParam("q", "shampoo").
+                when().
+                get("https://www.cosmostore.org/catalog/search").
+                then().assertThat().
+                statusCode(200).body(containsString("shampoo"));
+    }
+    @Test
+    @DisplayName("search 'Azzaro%20Coffret'")
+    public void testSearch2(){
+        given().
+                queryParam("q", "Azzaro%20Coffret").
+                when().
+                get("https://www.cosmostore.org/catalog/search").
+                then().assertThat().
+                statusCode(200).body(containsString("Azzaro%20Coffret"));
     }
 }
