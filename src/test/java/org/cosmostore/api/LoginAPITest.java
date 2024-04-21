ab0482de3797317eb56787c2cf75d4a3feaa3590
Victoria Vlasova <a5glyk@gmail.com>
2024-04-21 16:26:05 +0300

    refactor LoginAPITest

--- src/test/java/org/cosmostore/api/LoginAPITest.java
+++ src/test/java/org/cosmostore/api/LoginAPITest.java
@@ -39,5 +39,47 @@ public class LoginAPITest {
                 body(equalTo("{\"status\":\"error\",\"errors\":{\"message\":\"Invalid login information. " +
                         "Try to recover the password.\"}}"));
     }
+    @Test
+    @DisplayName("user with empty email")
+    public void testLogin3() {
+        given().
+                queryParam("location", "https%3A%2F%2Fwww.cosmostore.org%2F").
+                body("email=&password="+ UsersGenerator.generateEmailAndPasswordForUser().getPassword()+
+                        "+&submit_login=true").
+                contentType("application/x-www-form-urlencoded; charset=UTF-8").
+                when().
+                post("https://www.cosmostore.org/users/login/?location=https%3A%2F%2Fwww.cosmostore.org%2F").
+                then().assertThat().
+                statusCode(200).
+                body(equalTo("{\"status\":\"error\",\"errors\":{\"email\":\"Required field Email\"}}"));
+    }
+    @Test
+    @DisplayName("user with empty password")
+    public void testLogin4() {
+        given().
+                queryParam("location", "https%3A%2F%2Fwww.cosmostore.org%2F").
+                body("email="+UsersGenerator.generateEmailAndPasswordForUser().getEmail()+"&password=&submit_login=true").
+                contentType("application/x-www-form-urlencoded; charset=UTF-8").
+                when().
+                post("https://www.cosmostore.org/users/login/?location=https%3A%2F%2Fwww.cosmostore.org%2F").
+                then().assertThat().
+                statusCode(200).
+                body(equalTo("{\"status\":\"error\",\"errors\":{\"password\":\"Invalid login information. Try to recover the password.\"}}"));
+    }
+    @Test
+    @DisplayName("user with incorrect email")
+    public void testLogin5() {
+        given().
+                queryParam("location", "https%3A%2F%2Fwww.cosmostore.org%2F").
+                body("email=mkmopk&password="+ UsersGenerator.generateEmailAndPasswordForUser().getPassword()+
+                        "+&submit_login=true").
+                contentType("application/x-www-form-urlencoded; charset=UTF-8").
+                when().
+                post("https://www.cosmostore.org/users/login/?location=https%3A%2F%2Fwww.cosmostore.org%2F").
+                then().assertThat().
+                statusCode(200).
+                body(equalTo("{\"status\":\"error\",\"errors\":{\"email\":\"This is not the email\"}}"));
+    }
+
 }
 
